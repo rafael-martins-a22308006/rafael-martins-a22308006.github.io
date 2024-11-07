4476e18e9345b556b75bce9a09d6c6b7a9f137e7
Rafael <166533878+rafael-martins-a22308006@users.noreply.github.com>
2024-11-07 09:36:05 +0000

    mudei lab8

--- lab8/scripts/main.js
+++ lab8/scripts/main.js
@@ -1,7 +1,7 @@
 
-document.querySelectorAll("button.cor").forEach((e) => {
-    e.addEventListener("click", () => {
-        const color = e.dataset.color; // Acessa a cor armazenada em data-color
+document.querySelectorAll("button.cor").forEach((button) => {
+    button.addEventListener("click", () => {
+        const color = button.dataset.color; // Acessa a cor armazenada em data-color
         console.log("Cor selecionada:", color);
         document.getElementById("colorBox").style.backgroundColor = color; // Aplica a cor ao elemento
     });
